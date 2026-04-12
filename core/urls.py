3295438b32e1c04ec893bcb994dea35afaf7fce7
Lucas Biazoto <lucasmoreirabiazoto@hotmail.com>
2026-04-12 20:52:22 -0300

    add homepage with template

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Sistema Clínica Online 🚀")
+from .views import home
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]
